9d73baa21607b16f467bc33eccb128f609fa72b5
dcc <dccmx@dccmx.com>
2012-02-09 08:46:49 +0800

    better client report

--- client.c
+++ client.c
@@ -8,13 +8,14 @@
 #include <string.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <signal.h>
 
 #include "common.h"
 
-#define LOCK_INC(var) \
+#define LOCK_INC(var, v) \
   do{\
     pthread_mutex_lock(&stat_lock);\
-    var++;\
+    var+=v;\
     pthread_mutex_unlock(&stat_lock);\
   }while(0)
 
@@ -31,6 +32,8 @@ static int calc_success_count = 0;
 static int calc_count = 0;
 static int conn_success_count = 0;
 static int conn_fail_count = 0;
+static int total_time = 0;
+static int total_finish = 0;
 
 int nread(int fd, void *data, int size) {
   int ret, total = 0;
@@ -51,9 +54,10 @@ int nwrite(int fd, void *data, int size) {
 }
 
 void *do_a_plus_b(void *arg) {
-  int fd, i, on = 1;
+  int fd, i, on = 1, timeuse;
   struct sockaddr_in server_addr;
   struct timeval timeout={5, 0};
+  struct timeval start, end;
 
   fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (fd == -1 || setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
@@ -80,18 +84,20 @@ void *do_a_plus_b(void *arg) {
   }
   pthread_mutex_unlock(&init_count_mutex);
 
+  gettimeofday(&start, NULL);
+
   if (connect(fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
     perror("connect");
-    LOCK_INC(conn_fail_count);
+    LOCK_INC(conn_fail_count, 1);
     goto ERROR;
   }
   if (nread(fd, &i, sizeof(int)) == -1) {
     perror("connect");
-    LOCK_INC(conn_fail_count);
+    LOCK_INC(conn_fail_count, 1);
     goto ERROR;
   }
 
-  LOCK_INC(conn_success_count);
+  LOCK_INC(conn_success_count, 1);
 
   for (i = 0; i < NUM_CACL; i++) {
     int a, b, c, ta, tb;
@@ -99,7 +105,7 @@ void *do_a_plus_b(void *arg) {
     b = i + 1;
     ta = htonl(a);
     tb = htonl(b);
-    LOCK_INC(calc_count);
+    LOCK_INC(calc_count, 1);
     if (nwrite(fd, &ta, sizeof(int)) == -1 || nwrite(fd, &tb, sizeof(int)) == -1) {
       perror("write");
       goto ERROR;
@@ -107,28 +113,62 @@ void *do_a_plus_b(void *arg) {
     if (nread(fd, &c, sizeof(int)) == -1) {
       fprintf(stderr, "fd: %d ", fd);
       perror("read");
-      LOCK_INC(calc_count);
+      LOCK_INC(calc_count, 1);
       goto ERROR;
     }
     c = ntohl(c);
     if (c != a + b) {
       fprintf(stderr, "error: %d+%d=%d\n", a, b, c);
     } else {
-      LOCK_INC(calc_success_count);
+      LOCK_INC(calc_success_count, 1);
     }
   }
+
+  gettimeofday(&end, NULL );
+  timeuse = 1000 * ( end.tv_sec - start.tv_sec ) + (end.tv_usec - start.tv_usec) / 1000;
+  LOCK_INC(total_time, timeuse);
+  LOCK_INC(total_finish, 1);
 
 ERROR:
   close(fd);
   return NULL;
 }
 
+struct timeval start, end;
+
+void print_result() {
+  int timeuse;
+  gettimeofday(&end, NULL );
+  timeuse = 1000 * ( end.tv_sec - start.tv_sec ) + (end.tv_usec - start.tv_usec) / 1000;
+
+  if (total_finish > 0) total_time /= total_finish;
+
+  printf("\nconnect successfully %d\n"
+      "connect failed %d\n"
+      "calculate %d\n"
+      "calculate successfully %d\n"
+      "client avg. time %d.%ds\n"
+      "total time %d.%ds\n",
+      conn_success_count, conn_fail_count,
+      calc_count, calc_success_count,
+      total_time / 1000, total_time % 1000,
+      timeuse / 1000, timeuse % 1000);
+}
+
+void handle_signal(int signum) {
+  if (signum == SIGINT) {
+    print_result();
+    exit(0);
+  }
+}
+
 int main(int argc, char *argv[]) {
-  int i, timeuse;
+  int i;
   void *ret;
-  struct timeval start, end;
 
   server_ip = argv[1];
+
+  signal(SIGINT, handle_signal);
 
   gettimeofday(&start, NULL);
 
@@ -142,15 +182,5 @@ int main(int argc, char *argv[]) {
     pthread_join(threads[i], &ret);
   }
 
-  gettimeofday(&end, NULL );
-  timeuse = 1000 * ( end.tv_sec - start.tv_sec ) + (end.tv_usec - start.tv_usec) / 1000;
-
-  printf("\nconnect successfully %d\n"
-      "connect failed %d\n"
-      "calculate %d\n"
-      "calculate successfully %d\n"
-      "total time %d.%ds\n",
-      conn_success_count, conn_fail_count,
-      calc_count, calc_success_count,
-      timeuse / 1000, timeuse % 1000);
+  print_result();
 }
